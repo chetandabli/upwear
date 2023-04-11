94792673877b654ecb8360344197354f886a702b
chetandabli <hrku002@gmail.com>
2023-04-11 19:13:46 +0530

    Refactoring

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -9,24 +9,16 @@ const registrationSchema = mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    required: true,
-    match: [
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      "Please provide a valid email address",
-    ],
+    required: true
   },
   password: {
     type: String,
-    required: true,
-    match: [
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
-      "Password must contain at least one digit, one lowercase letter, one uppercase letter, and must be at least 8 characters long",
-    ],
+    required: true
   },
 });
 
 // Creating a model using the schema
-const RegistrationModel = mongoose.model("user", registrationSchema);
+const Registerationmodel = mongoose.model("user", registrationSchema);
 
 // Exporting the model
-module.exports = { RegistrationModel };
+module.exports = { Registerationmodel };
